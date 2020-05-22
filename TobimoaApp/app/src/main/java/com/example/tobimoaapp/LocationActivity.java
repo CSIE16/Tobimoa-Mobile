d830fc6a9724ce0dcfd71987e64d41979627dfc5
kyw04004 <kyw04004@naver.com>
2020-05-22 19:24:58 +0900

    connect server jsondata

--- TobimoaApp/app/src/main/java/com/example/tobimoaapp/LocationActivity.java
+++ TobimoaApp/app/src/main/java/com/example/tobimoaapp/LocationActivity.java
@@ -31,61 +31,22 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 public class LocationActivity extends AppCompatActivity
-        implements OnMapReadyCallback
-{
+        implements OnMapReadyCallback {
     private GoogleMap mMap;
     private Spinner spinner;
     ArrayList<String> arrayList;
     ArrayAdapter<String> arrayAdapter;
     JSONArray SD;
     StringBuffer sb = new StringBuffer();
-    int choose = 0;
-    String str =
-            "[{'Player':'홍길동','PhoneNum':'01012345678','SerialNum':'101','LocationX':'37.56','LocationY':'126.97','Visited1':'1','Visited2':'1','Visited3':'0','Visited4':'1'," +
-                    "'Visited5':'0','Visited6':'0','Visited7':'1','Visited8':'0','Visited9':'1'},"
-                    + "{'Player':'김영웅','PhoneNum':'01012341234','SerialNum':'102','LocationX':'37.57','LocationY':'126.98','Visited1':'0','Visited2':'0','Visited3':'0','Visited4':'1'," +
-                    "'Visited5':'1','Visited6':'1','Visited7':'1','Visited8':'0','Visited9':'0'},"
-                    + "{'Player':'황수민','PhoneNum':'01073900430','SerialNum':'103','LocationX':'37.58','LocationY':'126.99','Visited1':'1','Visited2':'0','Visited3':'1','Visited4':'1'," +
-                    "'Visited5':'1','Visited6':'1','Visited7':'1','Visited8':'0','Visited9':'0'}]";
-
+    int choose = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
-
-        arrayList = new ArrayList<>();
         new JSONTask().execute("http://tobimoa.ml/api/location/get");
-        try {
-            JSONArray array = new JSONArray(str);   // JSONArray 생성
-            for (int i = 0; i < array.length(); i++) {
-                JSONObject Object = array.getJSONObject(i);  // JSONObject 추출
-                String Player = Object.getString("Player");
-                arrayList.add(Player);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, arrayList);
-        spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setAdapter(arrayAdapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(), arrayList.get(i) + "이 선택되었습니다.",
-                        Toast.LENGTH_SHORT).show();
-                choose = i;
-                onMapReady(mMap);
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-            }
-        });
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync((OnMapReadyCallback) this);
-
         ImageButton button1 = findViewById(R.id.homeButton);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -119,7 +80,7 @@ public class LocationActivity extends AppCompatActivity
         protected String doInBackground(String... urls) {
             try {
                 JSONObject body = new JSONObject();
-                body.put("PhoneNum", "01073900430");
+                body.put("PhoneNum", "1234");
                 HttpURLConnection conn = null;
                 StringBuffer response = new StringBuffer();
                 try {
@@ -159,170 +120,183 @@ public class LocationActivity extends AppCompatActivity
             }
             return null;
         }
+
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
             try {
                 SD = new JSONArray(result);
-                System.out.println("성공"+SD);
-            }catch (JSONException e) {
+                System.out.println("성공" + SD);
+                arrayList = new ArrayList<>();
+                for (int i = 0; i < SD.length(); i++) {
+                    JSONObject Object = SD.getJSONObject(i);  // JSONObject 추출
+                    String Name = Object.getString("Name");
+                    arrayList.add(Name);
+                }
+                arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, arrayList);
+                spinner = (Spinner) findViewById(R.id.spinner);
+                spinner.setAdapter(arrayAdapter);
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        Toast.makeText(getApplicationContext(), arrayList.get(i) + "이 선택되었습니다.",
+                                Toast.LENGTH_SHORT).show();
+                        choose = i;
+                        onMapReady(mMap);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+                    }
+                });
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
-
     }
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
+        if(choose == -1) return;
         try {
             mMap.clear();
-            JSONArray jarray = new JSONArray(str);   // JSONArray 생성
-            JSONObject jObject = jarray.getJSONObject(choose);  // JSONObject 추출
-            String Player = jObject.getString("Player");
-            String SerialNum = jObject.getString("SerialNum");
-            String LocationX = jObject.getString("LocationX");
-            String LocationY = jObject.getString("LocationY");
-            String Visited1 = jObject.getString("Visited1");
-            String Visited2 = jObject.getString("Visited2");
-            String Visited3 = jObject.getString("Visited3");
-            String Visited4 = jObject.getString("Visited4");
-            String Visited5 = jObject.getString("Visited5");
-            String Visited6 = jObject.getString("Visited6");
-            String Visited7 = jObject.getString("Visited7");
-            String Visited8 = jObject.getString("Visited8");
-            String Visited9 = jObject.getString("Visited9");
+            JSONObject jObject = SD.getJSONObject(choose);  // JSONObject 추출
+            System.out.println("ㅎㅎㅎㅎㅎㅎㅎ확" + SD);
+            JSONObject lObject = jObject.getJSONObject("Location");  // JSONObject 추출
+            JSONArray vObject = jObject.getJSONArray("Visited");  // JSONObject 추출
+            String Player = jObject.getString("Name");
+            double LocationX = lObject.getDouble("latitude");
+            double LocationY = lObject.getDouble("longitude");
+            int Visited1 = vObject.optInt(1);
+            int Visited2 = vObject.optInt(2);
+            int Visited3 = vObject.optInt(3);
+            int Visited4 = vObject.optInt(4);
+            int Visited5 = vObject.optInt(5);
+            int Visited6 = vObject.optInt(6);
+            int Visited7 = vObject.optInt(7);
+            int Visited8 = vObject.optInt(8);
+            int Visited9 = vObject.optInt(9);
 
-            LatLng stamp1 = new LatLng(37.5,127);
+            LatLng stamp1 = new LatLng(37.5, 127);
             MarkerOptions markerOptions1 = new MarkerOptions();
             markerOptions1.position(stamp1);
             markerOptions1.title("1st Stamp Here");
-            if(Visited1.equals("1")) {
+            if (Visited1==1) {
                 markerOptions1.snippet("Success!!");
                 markerOptions1.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            }
-            else {
+            } else {
                 markerOptions1.snippet("Not yet...");
                 markerOptions1.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
             }
             mMap.addMarker(markerOptions1);
 
-            LatLng stamp2 = new LatLng(37.6,126.99);
+            LatLng stamp2 = new LatLng(37.6, 126.99);
             MarkerOptions markerOptions2 = new MarkerOptions();
             markerOptions2.position(stamp2);
             markerOptions2.title("2nd Stamp Here");
-            if(Visited2.equals("1")) {
+            if (Visited2==1) {
                 markerOptions2.snippet("Success!!");
                 markerOptions2.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            }
-            else {
+            } else {
                 markerOptions2.snippet("Not yet...");
                 markerOptions2.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
             }
             mMap.addMarker(markerOptions2);
 
-            LatLng stamp3 = new LatLng((37.55),126.95);
+            LatLng stamp3 = new LatLng((37.55), 126.95);
             MarkerOptions markerOptions3 = new MarkerOptions();
             markerOptions3.position(stamp3);
             markerOptions3.title("3rd Stamp Here");
-            if(Visited3.equals("1")) {
+            if (Visited3==1) {
                 markerOptions3.snippet("Success!!");
                 markerOptions3.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            }
-            else {
+            } else {
                 markerOptions3.snippet("Not yet...");
                 markerOptions3.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
             }
             mMap.addMarker(markerOptions3);
 
-            LatLng stamp4 = new LatLng((37.59),126.91);
+            LatLng stamp4 = new LatLng((37.59), 126.91);
             MarkerOptions markerOptions4 = new MarkerOptions();
             markerOptions4.position(stamp4);
             markerOptions4.title("4th Stamp Here");
-            if(Visited4.equals("1")) {
+            if (Visited4==1) {
                 markerOptions4.snippet("Success!!");
                 markerOptions4.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            }
-            else {
+            } else {
                 markerOptions4.snippet("Not yet...");
                 markerOptions4.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
             }
             mMap.addMarker(markerOptions4);
 
-            LatLng stamp5 = new LatLng((37.57),126.943);
+            LatLng stamp5 = new LatLng((37.57), 126.943);
             MarkerOptions markerOptions5 = new MarkerOptions();
             markerOptions5.position(stamp5);
             markerOptions5.title("5th Stamp Here");
-            if(Visited5.equals("1")) {
+            if (Visited5==1) {
                 markerOptions5.snippet("Success!!");
-                markerOptions5.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            }
-            else {
+            } else {
                 markerOptions5.snippet("Not yet...");
                 markerOptions5.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
             }
             mMap.addMarker(markerOptions5);
 
-            LatLng stamp6 = new LatLng((37.49),126.939);
+            LatLng stamp6 = new LatLng((37.49), 126.939);
             MarkerOptions markerOptions6 = new MarkerOptions();
             markerOptions6.position(stamp6);
             markerOptions6.title("6th Stamp Here");
-            if(Visited6.equals("1")) {
+            if (Visited6==1) {
                 markerOptions6.snippet("Success!!");
                 markerOptions6.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            }
-            else {
+            } else {
                 markerOptions6.snippet("Not yet...");
                 markerOptions6.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
             }
             mMap.addMarker(markerOptions6);
 
-            LatLng stamp7 = new LatLng((37.44),126.9);
+            LatLng stamp7 = new LatLng((37.44), 126.9);
             MarkerOptions markerOptions7 = new MarkerOptions();
             markerOptions7.position(stamp7);
             markerOptions7.title("7th Stamp Here");
-            if(Visited7.equals("1")) {
+            if (Visited7==1) {
                 markerOptions7.snippet("Success!!");
                 markerOptions7.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            }
-            else {
+            } else {
                 markerOptions7.snippet("Not yet...");
                 markerOptions7.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
             }
             mMap.addMarker(markerOptions7);
 
-            LatLng stamp8 = new LatLng((37.57),126.942);
+            LatLng stamp8 = new LatLng((37.67), 126.922);
             MarkerOptions markerOptions8 = new MarkerOptions();
             markerOptions8.position(stamp8);
             markerOptions8.title("8th Stamp Here");
-            if(Visited8.equals("1")) {
+            if (Visited8==1) {
                 markerOptions8.snippet("Success!!");
                 markerOptions8.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            }
-            else {
+            } else {
                 markerOptions8.snippet("Not yet...");
                 markerOptions8.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
             }
             mMap.addMarker(markerOptions8);
 
-            LatLng stamp9 = new LatLng((37.58),126.937);
+            LatLng stamp9 = new LatLng((37.58), 126.937);
             MarkerOptions markerOptions9 = new MarkerOptions();
             markerOptions9.position(stamp9);
             markerOptions9.title("9th Stamp Here");
-            if(Visited9.equals("1")) {
+            if (Visited9==1) {
                 markerOptions9.snippet("Success!!");
                 markerOptions9.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            }
-            else {
+            } else {
                 markerOptions9.snippet("Not yet...");
                 markerOptions9.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
             }
             mMap.addMarker(markerOptions9);
 
-            LatLng LOCATION = new LatLng(Double.parseDouble(LocationX),Double.parseDouble(LocationY));
+            LatLng LOCATION = new LatLng(LocationX, LocationY);
             MarkerOptions markerOptions = new MarkerOptions();
             markerOptions.position(LOCATION);
             markerOptions.title(Player);
-            markerOptions.snippet(SerialNum);
             markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.babysmall));
             mMap.addMarker(markerOptions);
             mMap.moveCamera(CameraUpdateFactory.newLatLng(LOCATION));
@@ -332,4 +306,3 @@ public class LocationActivity extends AppCompatActivity
         }
     }
 }
-
