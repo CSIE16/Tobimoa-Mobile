9e69f805487953ce382c13683e0075c4036158a8
kyw04004 <kyw04004@naver.com>
2020-05-24 17:09:24 +0900

    simplification of locationpage code

--- TobimoaApp/app/src/main/java/com/example/tobimoaapp/LocationActivity.java
+++ TobimoaApp/app/src/main/java/com/example/tobimoaapp/LocationActivity.java
@@ -111,11 +111,9 @@ public class LocationActivity extends AppCompatActivity
                     outputStream.writeBytes(body.toString());
                     outputStream.flush();
                     outputStream.close();//보낸 데이터 다음에 response써야함 안그러면 에러
-
                     InputStream stream = conn.getInputStream();
                     BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
                     String line = null;
-
                     while ((line = reader.readLine()) != null) {
                         response.append(line);
                     }
@@ -140,11 +138,13 @@ public class LocationActivity extends AppCompatActivity
             try {
                 SD = new JSONArray(result);
                 System.out.println("성공" + SD);
-                arrayList = new ArrayList<>();
-                for (int i = 0; i < SD.length(); i++) {
-                    JSONObject Object = SD.getJSONObject(i);  // JSONObject 추출
-                    String Name = Object.getString("Name");
-                    arrayList.add(Name);
+                if (choose == -1) {
+                    arrayList = new ArrayList<>();
+                    for (int i = 0; i < SD.length(); i++) {
+                        JSONObject Object = SD.getJSONObject(i);  // JSONObject 추출
+                        String Name = Object.getString("Name");
+                        arrayList.add(Name);
+                    }
                 }
                 arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, arrayList);
                 spinner = (Spinner) findViewById(R.id.spinner);
@@ -152,7 +152,7 @@ public class LocationActivity extends AppCompatActivity
                 spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                     @Override
                     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                        if(choose != past) {
+                        if (choose != past) {
                             Toast.makeText(getApplicationContext(), arrayList.get(i) + "이 선택되었습니다.",
                                     Toast.LENGTH_SHORT).show();
                         }
@@ -182,133 +182,37 @@ public class LocationActivity extends AppCompatActivity
             String Player = jObject.getString("Name");
             double LocationX = lObject.getDouble("latitude");
             double LocationY = lObject.getDouble("longitude");
-            int Visited1 = vObject.optInt(1);
-            int Visited2 = vObject.optInt(2);
-            int Visited3 = vObject.optInt(3);
-            int Visited4 = vObject.optInt(4);
-            int Visited5 = vObject.optInt(5);
-            int Visited6 = vObject.optInt(6);
-            int Visited7 = vObject.optInt(7);
-            int Visited8 = vObject.optInt(8);
-            int Visited9 = vObject.optInt(9);
+            int[] Visited = new int[10];
+            for(int i=1;i<=9;i++) Visited[1] = vObject.optInt(i);
 
-
-            LatLng stamp1 = new LatLng(37.5, 127);
-            MarkerOptions markerOptions1 = new MarkerOptions();
-            markerOptions1.position(stamp1);
-            markerOptions1.title("1st Stamp Here");
-            if (Visited1==1) {
-                markerOptions1.snippet("Success!!");
-                markerOptions1.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            } else {
-                markerOptions1.snippet("Not yet...");
-                markerOptions1.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
+            MarkerOptions markerOption = new MarkerOptions();
+            LatLng stamp[] = new LatLng[10];
+            stamp[1] = new LatLng(37.5,127);
+            stamp[2] = new LatLng(37.6,126.99);
+            stamp[3] = new LatLng(37.57,126.95);
+            stamp[4] = new LatLng(37.59,126.91);
+            stamp[5] = new LatLng(37.57,126.943);
+            stamp[6] = new LatLng(37.49,126.939);
+            stamp[7] = new LatLng(37.44,126.9);
+            stamp[8] = new LatLng(37.67,126.922);
+            stamp[9] = new LatLng(37.58,126.937);
+            //스탬프정
+            for(int i=1; i<=9; i++) {
+                if (i == 1) markerOption.title("1st Stamp Here");
+                else if (i == 2) markerOption.title("2nd Stamp Here");
+                else if (i == 3) markerOption.title("3rd Stamp Here");
+                else markerOption.title(i+"th Stamp Here");
+                markerOption.position(stamp[i]);
+                if (Visited[1] == 1) {
+                    markerOption.snippet("Success!!");
+                    markerOption.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
+                } else {
+                    markerOption.snippet("Not yet...");
+                    markerOption.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
+                }
+                mMap.addMarker(markerOption);
             }
-            mMap.addMarker(markerOptions1);
-
-            LatLng stamp2 = new LatLng(37.6, 126.99);
-            MarkerOptions markerOptions2 = new MarkerOptions();
-            markerOptions2.position(stamp2);
-            markerOptions2.title("2nd Stamp Here");
-            if (Visited2==1) {
-                markerOptions2.snippet("Success!!");
-                markerOptions2.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            } else {
-                markerOptions2.snippet("Not yet...");
-                markerOptions2.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
-            }
-            mMap.addMarker(markerOptions2);
-
-            LatLng stamp3 = new LatLng((37.55), 126.95);
-            MarkerOptions markerOptions3 = new MarkerOptions();
-            markerOptions3.position(stamp3);
-            markerOptions3.title("3rd Stamp Here");
-            if (Visited3==1) {
-                markerOptions3.snippet("Success!!");
-                markerOptions3.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            } else {
-                markerOptions3.snippet("Not yet...");
-                markerOptions3.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
-            }
-            mMap.addMarker(markerOptions3);
-
-            LatLng stamp4 = new LatLng((37.59), 126.91);
-            MarkerOptions markerOptions4 = new MarkerOptions();
-            markerOptions4.position(stamp4);
-            markerOptions4.title("4th Stamp Here");
-            if (Visited4==1) {
-                markerOptions4.snippet("Success!!");
-                markerOptions4.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            } else {
-                markerOptions4.snippet("Not yet...");
-                markerOptions4.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
-            }
-            mMap.addMarker(markerOptions4);
-
-            LatLng stamp5 = new LatLng((37.57), 126.943);
-            MarkerOptions markerOptions5 = new MarkerOptions();
-            markerOptions5.position(stamp5);
-            markerOptions5.title("5th Stamp Here");
-            if (Visited5==1) {
-                markerOptions5.snippet("Success!!");
-            } else {
-                markerOptions5.snippet("Not yet...");
-                markerOptions5.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
-            }
-            mMap.addMarker(markerOptions5);
-
-            LatLng stamp6 = new LatLng((37.49), 126.939);
-            MarkerOptions markerOptions6 = new MarkerOptions();
-            markerOptions6.position(stamp6);
-            markerOptions6.title("6th Stamp Here");
-            if (Visited6==1) {
-                markerOptions6.snippet("Success!!");
-                markerOptions6.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            } else {
-                markerOptions6.snippet("Not yet...");
-                markerOptions6.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
-            }
-            mMap.addMarker(markerOptions6);
-
-            LatLng stamp7 = new LatLng((37.44), 126.9);
-            MarkerOptions markerOptions7 = new MarkerOptions();
-            markerOptions7.position(stamp7);
-            markerOptions7.title("7th Stamp Here");
-            if (Visited7==1) {
-                markerOptions7.snippet("Success!!");
-                markerOptions7.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            } else {
-                markerOptions7.snippet("Not yet...");
-                markerOptions7.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
-            }
-            mMap.addMarker(markerOptions7);
-
-            LatLng stamp8 = new LatLng((37.67), 126.922);
-            MarkerOptions markerOptions8 = new MarkerOptions();
-            markerOptions8.position(stamp8);
-            markerOptions8.title("8th Stamp Here");
-            if (Visited8==1) {
-                markerOptions8.snippet("Success!!");
-                markerOptions8.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            } else {
-                markerOptions8.snippet("Not yet...");
-                markerOptions8.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
-            }
-            mMap.addMarker(markerOptions8);
-
-            LatLng stamp9 = new LatLng((37.58), 126.937);
-            MarkerOptions markerOptions9 = new MarkerOptions();
-            markerOptions9.position(stamp9);
-            markerOptions9.title("9th Stamp Here");
-            if (Visited9==1) {
-                markerOptions9.snippet("Success!!");
-                markerOptions9.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
-            } else {
-                markerOptions9.snippet("Not yet...");
-                markerOptions9.icon(BitmapDescriptorFactory.fromResource(R.drawable.starsmall));
-            }
-            mMap.addMarker(markerOptions9);
-
+            //아이위치정보
             LatLng LOCATION = new LatLng(LocationX, LocationY);
             MarkerOptions markerOptions = new MarkerOptions();
             markerOptions.position(LOCATION);
