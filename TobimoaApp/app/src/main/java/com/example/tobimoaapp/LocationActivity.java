8dedb6346e2828bc438d637ddfc0a8d713734e09
kyw04004 <kyw04004@naver.com>
2020-05-29 02:06:39 +0900

    add function my location(location of phone)

--- TobimoaApp/app/src/main/java/com/example/tobimoaapp/LocationActivity.java
+++ TobimoaApp/app/src/main/java/com/example/tobimoaapp/LocationActivity.java
@@ -1,18 +1,25 @@
 package com.example.tobimoaapp;
 import androidx.appcompat.app.AppCompatActivity;
-import android.app.Activity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
-import com.example.tobimoaapp.LoginActivity;
-import com.example.tobimoaapp.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,9 +27,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -30,25 +39,29 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.CollationElementIterator;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
+
 public class LocationActivity extends AppCompatActivity
         implements OnMapReadyCallback {
+    double latitude ;
+    double longitude ;
     private GoogleMap mMap;
     private Spinner spinner;
     ArrayList<String> arrayList;
     ArrayAdapter<String> arrayAdapter;
     JSONArray SD;
     StringBuffer sb = new StringBuffer();
-    int cnt=0;
-    int now=-3;
-    int choose = -1;
-    int past = -2;
+    int cnt = 0, now = -3, choose = -1, past = -2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        startLocationService();
         TimerTask tt = new TimerTask() {
             @Override
             public void run() {
@@ -58,8 +71,7 @@ public class LocationActivity extends AppCompatActivity
             }
         };
         Timer timer = new Timer();
-        timer.schedule(tt,0,10000);
-
+        timer.schedule(tt, 0, 10000);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync((OnMapReadyCallback) this);
@@ -157,19 +169,24 @@ public class LocationActivity extends AppCompatActivity
                     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                         if (past == -2) // 0으로 처음 셋팅
                         {
-                            Toast.makeText(getApplicationContext(), arrayList.get(i) + "이 선택되었습니다.",
+                            Toast.makeText(getApplicationContext(), arrayList.get(i),
                                     Toast.LENGTH_SHORT).show();
                             choose = i;
                         } else if (now == cnt) // 선택할 때
                         {
-                            Toast.makeText(getApplicationContext(), arrayList.get(i) + "이 선택되었습니다.",
-                                    Toast.LENGTH_SHORT).show();
+                            if (i == 0)
+                                Toast.makeText(getApplicationContext(), arrayList.get(i),
+                                        Toast.LENGTH_SHORT).show();
+                            else
+                                Toast.makeText(getApplicationContext(), arrayList.get(i) + "이 선택되었습니다.",
+                                        Toast.LENGTH_SHORT).show();
                             choose = i;
-                            if(choose > 0) choose--;
+                            if (choose > 0) choose--;
                         }
                         System.out.println(i);
                         onMapReady(mMap);
                     }
+
                     @Override
                     public void onNothingSelected(AdapterView<?> adapterView) {
                     }
@@ -184,7 +201,7 @@ public class LocationActivity extends AppCompatActivity
     @Override
     public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
-        if(choose == -1) return;
+        if (choose == -1) return;
         try {
             mMap.clear();
             JSONObject jObject = SD.getJSONObject(choose);  // JSONObject 추출
@@ -195,27 +212,27 @@ public class LocationActivity extends AppCompatActivity
             double LocationX = lObject.getDouble("latitude");
             double LocationY = lObject.getDouble("longitude");
             int[] Visited = new int[10];
-            for(int i=1;i<=9;i++) Visited[1] = vObject.optInt(i);
+            for (int i = 1; i <= 9; i++) Visited[i] = vObject.optInt(i);
 
             MarkerOptions markerOption = new MarkerOptions();
             LatLng stamp[] = new LatLng[10];
-            stamp[1] = new LatLng(37.5,127);
-            stamp[2] = new LatLng(37.6,126.99);
-            stamp[3] = new LatLng(37.57,126.95);
-            stamp[4] = new LatLng(37.59,126.91);
-            stamp[5] = new LatLng(37.57,126.943);
-            stamp[6] = new LatLng(37.49,126.939);
-            stamp[7] = new LatLng(37.44,126.9);
-            stamp[8] = new LatLng(37.67,126.922);
-            stamp[9] = new LatLng(37.58,126.937);
+            stamp[1] = new LatLng(37.5, 127);
+            stamp[2] = new LatLng(37.6, 126.99);
+            stamp[3] = new LatLng(37.57, 126.95);
+            stamp[4] = new LatLng(37.59, 126.91);
+            stamp[5] = new LatLng(37.57, 126.943);
+            stamp[6] = new LatLng(37.49, 126.939);
+            stamp[7] = new LatLng(37.44, 126.9);
+            stamp[8] = new LatLng(37.67, 126.922);
+            stamp[9] = new LatLng(37.58, 126.937);
             //스탬프정보
-            for(int i=1; i<=9; i++) {
+            for (int i = 1; i <= 9; i++) {
                 if (i == 1) markerOption.title("1st Stamp Here");
                 else if (i == 2) markerOption.title("2nd Stamp Here");
                 else if (i == 3) markerOption.title("3rd Stamp Here");
-                else markerOption.title(i+"th Stamp Here");
+                else markerOption.title(i + "th Stamp Here");
                 markerOption.position(stamp[i]);
-                if (Visited[1] == 1) {
+                if (Visited[i] == 1) {
                     markerOption.snippet("Success!!");
                     markerOption.icon(BitmapDescriptorFactory.fromResource(R.drawable.flagsmall));
                 } else {
@@ -224,6 +241,13 @@ public class LocationActivity extends AppCompatActivity
                 }
                 mMap.addMarker(markerOption);
             }
+            //핸드폰위치정보
+            LatLng pLOCATION = new LatLng(latitude, longitude);
+            MarkerOptions pmarkerOptions = new MarkerOptions();
+            pmarkerOptions.position(pLOCATION);
+            pmarkerOptions.title("Me");
+            pmarkerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.babysmall));
+            mMap.addMarker(pmarkerOptions);
             //아이위치정보
             LatLng LOCATION = new LatLng(LocationX, LocationY);
             MarkerOptions markerOptions = new MarkerOptions();
@@ -231,7 +255,7 @@ public class LocationActivity extends AppCompatActivity
             markerOptions.title(Player);
             markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.babysmall));
             mMap.addMarker(markerOptions);
-            if(choose != past) {
+            if (choose != past) {
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(LOCATION));
                 mMap.animateCamera(CameraUpdateFactory.zoomTo(18));
                 past = choose;
@@ -239,6 +263,53 @@ public class LocationActivity extends AppCompatActivity
             now = cnt;
         } catch (JSONException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void startLocationService() {
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        try {
+            Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location != null) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                String message = "최근 위치 -> Latitude: " + latitude + "\nLongitude:" + longitude;
+                System.out.println(message);
+            } else System.out.println("실패애ㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐ");
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+        GPSListener gpsListener = new GPSListener();
+        long minTime = 1000;
+        float minDistance = 0;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, gpsListener);
+    }
+    class GPSListener implements LocationListener {
+        public void onLocationChanged(Location location) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+            String message = "최근 위치 -> Latitude: " + latitude + "\nLongitude:" + longitude;
+            System.out.println(message);
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
         }
     }
 }
